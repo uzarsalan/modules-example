a000e4da92c04c6c29898ab85051690bd72931e4
Arsalan Rabdanov <uzarsalan@gmail.com>
2022-12-01 13:32:25 +0300

    first comit

---
+++ age.js
@@ -0,0 +1,4 @@
+//age.js
+export let birthday = "01.01.2000";
+let age = 32;
+export default age;
---
+++ user.js
@@ -0,0 +1,7 @@
+//user.js
+export let name = "Ваня";
+let age = 32;
+
+export function hello(userName) {
+	alert(`Приветствую, ${userName}`)
+}
